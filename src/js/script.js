c97f244fda738f5fa0e35a9d9aa0627b4375040e
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2025-11-07 09:07:48 +0100

    Ouverture / Fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,14 @@ const toggle = document.querySelector('.header__menu-btn');
 const nav = document.querySelector('.header__menu');
 
 toggle.addEventListener("click", () => {
-  // Mise à jour des attributs ARIA pour accessibilité
-  nav.ariaHidden = "true" ? "false" : "true" ;
-  toggle.ariaExpanded = "false" ? "true" : "false" ;
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    if (isClosed) {
+        toggle.ariaExpanded = "true";
+        nav.ariaHidden = "false";
+    } else {
+        toggle.ariaExpanded = "false";
+        nav.ariaHidden = "true";
+    }
 });
-
